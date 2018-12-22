01d3e6ea20c91a5b88c035f9f203ebe18f0a4fc1
Angelo Martin <angeloqmartin@gmail.com>
2018-12-22 13:20:12 -0800

    updated server.js

--- server.js
+++ server.js
@@ -48,8 +48,6 @@ app.get('/Recipes', (req, res) => {
   res.json(Recipes.get());
 });
 
-
-
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
